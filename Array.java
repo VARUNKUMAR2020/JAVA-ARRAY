0db29e64927664c42dab06f131bd747a4fad7e74
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 16:13:41 +0530

    ADD: Duplicate numbers in array

--- Array.java
+++ Array.java
@@ -3,17 +3,14 @@ public class Array {
         System.out.println("Largest Number in the array: ");
 
         // array
-        int number[] = { 23, 24, 54, 65, 100, 145, 654 };
-
-        // assign
-        int max = number[0];
-
-        // to run the elements in the array
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] < max) {
-                max = number[i];
+        int number[] = { 23, 24, 100, 65, 24, 54, 65, 100, 145, 654 };
+        for (int i = 0; i < number.length - 1; i++) {
+            for (int j = i + 1; j < number.length; j++) {
+                if ((number[i] == number[j])) {
+                    System.out.println("Duplicate Elements are: " + number[j]);
+                }
             }
         }
-        System.out.println("Largest number is: " + max);
+
     }
 }
