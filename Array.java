e7a758800ec25a332bd1ecd921045f7228c3402e
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 15:55:41 +0530

    ADD: Rverse the elements in array

--- Array.java
+++ Array.java
@@ -1,19 +1,14 @@
 public class Array {
     public static void main(String[] args) throws Exception {
-        System.out.println("Largest Number in the array: ");
+        System.out.println("Reverse order in the array: ");
 
         // array
         int number[] = { 23, 24, 54, 65, 100, 145, 654 };
 
-        // assign
-        int max = number[0];
-
         // to run the elements in the array
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] < max) {
-                max = number[i];
-            }
+        for (int i=number.length-1;i>=0;i--) {
+            System.out.print(number[i]+" ");
         }
-        System.out.println("Largest number is: " + max);
+        
     }
 }
