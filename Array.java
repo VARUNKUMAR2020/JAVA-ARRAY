32c9fc6e1ffd3997147c7bdaf35441a04797acf0
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 15:32:35 +0530

    ADD: To find largest number

--- Array.java
+++ Array.java
@@ -1,5 +1,19 @@
 public class Array {
     public static void main(String[] args) throws Exception {
-        System.out.println("Array in Java");
+        System.out.println("Largest Number in the array: ");
+
+        // array
+        int number[] = { 23, 24, 54, 65, 100, 145, 654 };
+
+        // assign
+        int max = number[0];
+
+        // to run the elements in the array
+        for (int i = 0; i < number.length; i++) {
+            if (number[i] > max) {
+                max = number[i];
+            }
+        }
+        System.out.println("Largest number is: " + max);
     }
 }
