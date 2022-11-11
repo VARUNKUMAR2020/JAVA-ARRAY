f8ebab210318cc82eee7541798f251c1af64c6a9
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 16:03:18 +0530

    ADD: Odd place of the number

--- Array.java
+++ Array.java
@@ -1,19 +1,17 @@
 public class Array {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Largest Number in the array: ");
+    public static void main(String[] args) {
+        System.out.println("Even place Number in the array: ");
 
         // array
         int number[] = { 23, 24, 54, 65, 100, 145, 654 };
 
-        // assign
-        int max = number[0];
-
         // to run the elements in the array
-        for (int i = 0; i < number.length; i++) {
-            if (number[i] < max) {
-                max = number[i];
+        for (int i = 1; i < number.length; i++) {
+
+            //condition to check even place
+            if (i % 2 != 0) {
+                System.out.println(number[i]);
             }
         }
-        System.out.println("Largest number is: " + max);
     }
 }
