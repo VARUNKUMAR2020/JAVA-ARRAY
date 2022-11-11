baf49249e93d9c05296909ef96ca17539522ed50
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-11 14:37:59 +0530

    ADD: Print the elements

--- Array.java
+++ Array.java
@@ -1,5 +1,10 @@
 public class Array {
     public static void main(String[] args) throws Exception {
-        System.out.println("Array in Java");
+        System.out.println("Print the elements in array: ");
+        int array[]={1,2,3,4,5,6,7,8,9,10};
+        for(int i=0;i<array.length;i++){
+                System.out.println(array[i]);
+        }
+
     }
 }
